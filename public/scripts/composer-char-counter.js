4813f228355de83962677f126f4caa74b4659219
Neerav Patel <nickpatel.developer@gmail.com>
2021-05-18 13:11:07 -0400

    Implement the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  console.log("Hello");
+
+  const updateCounter = function(element, counter) {
+    const counterElement = $(element).parent().children("div.action").children("output.counter");
+    const limit = 140;
+    let result = limit - counter;
+    if (result < 0) {
+      counterElement.css("color","#f11");
+    } else {
+      counterElement.css("color","#545149");
+    }
+    counterElement.text(result);
+  }
+
+  $(".new-tweet textarea").on("input", function() {
+    let counter  = $(this).val().length;
+    updateCounter(this, counter);
+  })
+  
+});
